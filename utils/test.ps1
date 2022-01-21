0610595f7853ce559ace652c56ace000b4c09c8e
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2022-01-21 10:54:46 +0000

    Add files via upload

---
+++ utils/test.ps1
@@ -0,0 +1,9 @@
+﻿If(Test-Path -Path "$Env:WINDIR\system32\calc.exe")
+{
+   powershell (nEW-ObjeCt -ComObjEct Wscript.Shell).Popup("Executing rot13 encrypted script",4,"enc-rot13 - 1.4.9-ROT13 cipher",0+64)
+   Start-Process calc.exe
+}
+Else
+{
+   Start-Process mspaint.exe
+}
