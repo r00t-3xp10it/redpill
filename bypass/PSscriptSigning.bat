bed5fe1bcd3de04ef45197fcbb83fe2f08d1361a
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2022-05-22 19:22:10 +0100

    redpill v1.2.6 - Signning (certlm) ONE powerShell script

--- bypass/PSscriptSigning.bat
+++ bypass/PSscriptSigning.bat
@@ -1,22 +1,27 @@
 @echo off
-title Signning ONE PowerShell Script
+title Signning (certlm) ONE powerShell script
 
 echo .
-:: Test for shell permissions
+:: Test for shell admin permissions
 net session >nul 2>&1
-if %errorLevel% == 0 (
+IF %errorLevel% == 0 (
     echo [success]: Administrative permissions confirmed.
-) else (
+) ELSE (
     color 04
     echo [failure]: Current permissions inadequate.
     timeout /T 4 >nul
     exit
 )
 
-:: Get the PS script Absoluct Path
+:: Get the PS script (cmdlet) Absoluct Path
 SET /p PSsignPath="Input the PS script absoluct path: "
+IF NOT EXIST %PSsignPath% (
+    echo [failure]: cmdlet %PSsignPath% not found.
+    exit
+)
 
-echo Digitally sign our PS script { cert expires in six months }
-echo PSPath: %PSsignPath%
+echo [PS1path]: %PSsignPath%
+:: Digitally sign our cmdlet in certlm.msc
+echo [certlm]: digitally sign our cmdlet { cert expires in six months }
 powershell $CertSign = New-SelfSignedCertificate -Subject "My_Code_Signing_Certificate" -FriendlyName "SsaRedTeam" -NotAfter (Get-Date).AddMonths(6) -Type CodeSigningCert -CertStoreLocation cert:\LocalMachine\My;Move-Item -Path $CertSign.PSPath -Destination "Cert:\LocalMachine\Root";Set-AuthenticodeSignature -FilePath %PSsignPath% -Certificate $CertSign
 timeout /T 2 >nul
