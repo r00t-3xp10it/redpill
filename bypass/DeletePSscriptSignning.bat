d762803ad4e138f56c3c8a1413ca1888df36032d
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2022-05-23 02:01:41 +0100

    redpill v1.2.6 - Signning (certlm) ONE powerShell script

--- bypass/DeletePSscriptSignning.bat
+++ bypass/DeletePSscriptSignning.bat
@@ -5,7 +5,7 @@
 ::   Description:
 ::      Delete Certificates from store
 :: --
-title Delete Certs from store
+title Delete Certificates from store
 
 
 :: Test for shell permissions
@@ -23,4 +23,4 @@ timeout /T 3 >nul
 
 echo [ exec  ]: Deleting 'SsaRedTeam' (FriendlyName) Certificate from Cert Store ..
 powershell $List = @('Root','My');ForEach($Item in $List){Get-ChildItem Cert:\LocalMachine\$Item ^| Where-Object {$_.Issuer -match 'My_Code_Signing_Certificate'} ^| Remove-Item}
-# powershell Get-ChildItem Cert:\LocalMachine\Root ^| Where-Object {$_.Issuer -match 'My_Code_Signing_Certificate'} ^| Remove-Item
+:: powershell Get-ChildItem Cert:\LocalMachine\Root ^| Where-Object {$_.Issuer -match 'My_Code_Signing_Certificate'} ^| Remove-Item
