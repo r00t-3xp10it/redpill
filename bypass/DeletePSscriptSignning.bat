c27dab5eed03087eba91da7da933bf861240637b
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2022-05-23 01:59:23 +0100

    redpill v1.2.6 - Signning (certlm) ONE powerShell script

--- bypass/DeletePSscriptSignning.bat
+++ bypass/DeletePSscriptSignning.bat
@@ -1,20 +1,26 @@
 @echo off
+::   Author:
+::      @r00t-3xp10it
+::
+::   Description:
+::      Delete Certificates from store
+:: --
 title Delete Certs from store
 
 
 :: Test for shell permissions
 net session >nul 2>&1
-if %errorLevel% == 0 (
+IF %errorLevel% == 0 (
     echo [success]: Administrative permissions confirmed.
 ) else (
-    color 04
     echo [failure]: Current permissions inadequate.
-    timeout /T 4 >nul
     exit
 )
 
-echo [i] Reverting %userdomain% to Previous State.
+echo [ exec  ]: Check for certificate existence.
+powershell $List = @('Root','My');ForEach($Item in $List){Get-ChildItem Cert:\LocalMachine\$Item ^| Where-Object {$_.Issuer -match 'My_Code_Signing_Certificate'}}
 timeout /T 3 >nul
-echo [i] Deleting 'SsaRedTeam' (FriendlyName) Certificate from Cert Store ..
-powershell Get-ChildItem Cert:\LocalMachine\Root ^| Where-Object {$_.Issuer -match 'My_Code_Signing_Certificate'}
-powershell Get-ChildItem Cert:\LocalMachine\Root ^| Where-Object {$_.Issuer -match 'My_Code_Signing_Certificate'} ^| Remove-Item
+
+echo [ exec  ]: Deleting 'SsaRedTeam' (FriendlyName) Certificate from Cert Store ..
+powershell $List = @('Root','My');ForEach($Item in $List){Get-ChildItem Cert:\LocalMachine\$Item ^| Where-Object {$_.Issuer -match 'My_Code_Signing_Certificate'} ^| Remove-Item}
+# powershell Get-ChildItem Cert:\LocalMachine\Root ^| Where-Object {$_.Issuer -match 'My_Code_Signing_Certificate'} ^| Remove-Item
