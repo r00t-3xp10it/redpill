244b89382b19e34be812e240657a4764c12c7001
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2022-05-24 14:14:42 +0100

    redpill v1.2.6 - RedTeam database library

--- lib/CertSign_PS1/Invoke-LazySign.ps1
+++ lib/CertSign_PS1/Invoke-LazySign.ps1
@@ -7,7 +7,7 @@
    Tested Under: Windows 10 (19043) x64 bits
    Required Dependencies: PSVersion 3 {native?}
    Optional Dependencies: Administrator privs
-   PS cmdlet Dev version: v1.0.4
+   PS cmdlet Dev version: v1.0.5
 
 .DESCRIPTION
    This cmdlet allow users to sign windows binarys or scripts
@@ -19,6 +19,8 @@
    This cmdlet uses 'Cert:\CurrentUser\My' store to create certs,
    Unless cmdlet its executed with administrator privileges. In that
    ocassion it exports the certificate to 'Cert:\LocalMachine\Root'.
+   Invoking -NotAfter '8' will maintain the fake cert in store for
+   the 8 months, and auto-delete it self after limmit time reached.
 
 .Parameter Action
    Accepts arguments: query, sign (default: query)
@@ -34,6 +36,9 @@
 
 .Parameter Subject
    Certificate subject name (default: LazySign)
+
+.Parameter NotAfter
+   Auto-Delete cert after [?]months (default: 1)
 
 .Parameter Password
    Certificate password (default: Passw0rd!)
@@ -62,6 +67,7 @@
       FriendlyName : SsaRedTeam
       DomainName   : microsoft.com
       Subject      : LazySign-4zrH
+      ExprireDate  : 24/05/2023 11:48:38
       CertLocation : Cert:\CurrentUser\My
       Target       : C:\Users\pedro\AppData\Local\Temp\Payload.ps1
       Password     : Passw0rd!
@@ -75,6 +81,7 @@
 .LINK
    https://github.com/jfmaes/LazySign
    https://sid-500.com/2017/10/26/how-to-digitally-sign-powershell-scripts
+   https://github.com/r00t-3xp10it/hacking-material-books/blob/master/obfuscation/working-with-certificates.md
 #>
 
 
@@ -84,14 +91,16 @@
    [string]$Password="Passw0rd!",
    [string]$Subject="LazySign",
    [string]$Action="query",
-   [string]$Target="off"
+   [string]$Target="off",
+   [int]$NotAfter='1'
 )
 
 
-$CmdletVersion = "v1.0.4"
+$CmdletVersion = "v1.0.5"
 #Global variable declarations
 $StoreLocation = "Cert:\CurrentUser\My"
 $ErrorActionPreference = "SilentlyContinue"
+
 #Disable Powershell Command Logging for current session.
 Set-PSReadlineOption –HistorySaveStyle SaveNothing|Out-Null
 $host.UI.RawUI.WindowTitle = "@Invoke-LazySign $CmdletVersion {SSA@RedTeam}"
@@ -113,7 +122,13 @@ If(-not(Test-Path -Path "$StoreLocation"))
    return
 }
 
-#Supported locations
+If($NotAfter -eq 0)
+{
+   #auto-del limmit
+   [int]$NotAfter='1'
+}
+
+#Supported stores
 $LocationsList = @(
    "Cert:\CurrentUser\My",
    "Cert:\LocalMachine\Root"
@@ -144,6 +159,7 @@ If($Action -ieq "query")
    * Exit Invoke-LazySign cmdlet [ok]..   
    #>
 
+   #Local function variable declarations
    write-host "  + " -ForegroundColor DarkYellow -NoNewline
    write-host "Store Location: " -ForegroundColor DarkGray -NoNewline
    write-host "$StoreLocation" -ForegroundColor DarkYellow
@@ -163,6 +179,7 @@ If($Action -ieq "query")
 
    If((Get-Content -Path "$Env:TMP\dave.log") -eq $null)
    {
+      Remove-Item -Path "$Env:TMP\dave.log" -Force
       write-host "`n  x Error: none '$Subject' certificates found..`n" -ForegroundColor Red -BackgroundColor Black
       write-host "* Exit Invoke-LazySign cmdlet [" -ForegroundColor Green -NoNewline
       write-host "ok" -ForegroundColor DarkYellow -NoNewline
@@ -199,6 +216,7 @@ If($Action -ieq "Sign")
         FriendlyName : SsaRedTeam
         DomainName   : microsoft.com
         Subject      : LazySign-4zrH
+        ExprireDate  : 24/05/2023 11:48:38
         CertLocation : Cert:\CurrentUser\My
         Target       : auxiliary.ps1
         Password     : Passw0rd!
@@ -211,8 +229,8 @@ If($Action -ieq "Sign")
    #>
 
    #Local function variable declarations
-   $SupportedVersion = ($PSversionTable).PSVersion.Major
    $CertPath = $(Join-Path (Get-Location) "$Domain.pfx")
+   $SupportedVersion = ($PSversionTable).PSVersion.Major
    $Rand = -join (((48..57)+(65..90)+(97..122)) * 80 |Get-Random -Count 4 |%{[char]$_})
    $bool = (([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -Match "S-1-5-32-544")
    $SecurePassword = ConvertTo-SecureString -String $Password -AsPlainText -Force -ErrorAction SilentlyContinue
@@ -256,20 +274,24 @@ If($Action -ieq "Sign")
       return
    }
 
+
    #New Certificate Settings Display
+   $ExpireTime = (Get-Date).AddMonths($NotAfter)
    $RandSubject = "$Subject" + "-" + "$Rand" -join ''
    write-host "`n  Certificate information" -ForegroundColor Green
    write-host "  -----------------------"
    write-host "  FriendlyName : $FriendlyName"
    write-host "  DomainName   : $Domain"
    write-host "  Subject      : $RandSubject" -ForegroundColor DarkYellow
+   write-host "  ExprireDate  : $ExpireTime"
    write-host "  CertLocation : $StoreLocation"
    write-host "  Target       : $Target" -ForegroundColor DarkGray
    write-host "  Password     : $Password`n"
    Start-Sleep -Seconds 1
 
+
    #Create Self Signed Certificate and PFX file in current directory
-   $Certificate = New-SelfSignedCertificate -Subject "$RandSubject" -FriendlyName "$FriendlyName" -CertStoreLocation "$StoreLocation" -DnsName "$Domain" -Type "CodeSigning" -ErrorAction SilentlyContinue  
+   $Certificate = New-SelfSignedCertificate -Subject "$RandSubject" -FriendlyName "$FriendlyName" -CertStoreLocation "$StoreLocation" -DnsName "$Domain" -NotAfter (Get-Date).AddMonths($NotAfter) -Type "CodeSigning" -ErrorAction SilentlyContinue 
    Export-PfxCertificate -FilePath "$CertPath" -Password "$SecurePassword" -Cert "$Certificate" -ErrorAction SilentlyContinue
 
    If(-not(Test-Path -Path "$CertPath"))
@@ -289,19 +311,30 @@ If($Action -ieq "Sign")
       .SYNOPSIS
          Author: @r00t-3xp10it
          Helper - Move certificate to 'Cert:\LocalMachine\Root'
+
+      .NOTES
+         Administrator privileges required to move cert
       #>
 
       $TestExistence = (Get-ChildItem "Cert:\CurrentUser\My"|?{$_.Issuer -iMatch "$Subject" -or $_.Subject -iMatch "^(CN=$Subject)"}).Subject
       If($TestExistence)
       {
-         write-host "   + Moving certificate to 'Cert:\LocalMachine\Root'" -ForegroundColor Green
-         Move-Item -Path $Certificate.PSPath -Destination "Cert:\LocalMachine\Root"
+         write-host "   + " -ForegroundColor DarkYellow -NoNewline
+         write-host "Moving certificate to '" -ForegroundColor DarkGray -NoNewline
+         write-host "Cert:\LocalMachine\Root" -ForegroundColor DarkYellow -NoNewline
+         write-host "'" -ForegroundColor DarkGray
+
+         #Move certificate
+         Move-Item -Path $Certificate.PSPath -Destination "Cert:\LocalMachine\Root" -Force
       }
    }
 
-   #Sign our Windows binary\cmdlet with a self-signed certificate
-   Set-AuthenticodeSignature -Certificate "$Certificate" -Filepath "$Target" –TimestampServer "http://timestamp.comodoca.com/authenticode"
+
+   ## Sign our Windows binary\cmdlet with a self-signed certificate
+   # needed? : –TimestampServer "http://timestamp.comodoca.com/authenticode" -Force
+   Set-AuthenticodeSignature -Certificate "$Certificate" -Filepath "$Target" -Force
    If(Test-Path -Path "$CertPath"){Remove-Item -Path "$CertPath" -Force}
+
 
    #Display query store settings - 30a40d0a
    ForEach($SetLocation in $LocationsList)
