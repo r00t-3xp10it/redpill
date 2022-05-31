f380c104abad65095fe6b58e1152151a387dc6e5
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2022-05-31 03:24:25 +0100

    redpill v1.2.6 - RedTeam database library

---
+++ lib/Misc/inspect-exe.ps1
@@ -0,0 +1,31 @@
+﻿<#
+.SYNOPSIS
+	Prints basic information of an executable file
+
+.DESCRIPTION
+	This PowerShell script prints basic information of an executable file.
+
+.PARAMETER PathToExe
+	Specifies the path to the executable file
+
+.EXAMPLE
+	PS> ./inspect-exe C:\MyApp.exe
+
+.LINK
+	https://github.com/fleschutz/PowerShell
+
+.NOTES
+	Author: Markus Fleschutz / License: CC0
+#>
+
+param([string]$PathToExe = "")
+
+try {
+	if ($PathToExe -eq "" ) { $PathToExe = read-host "Enter path to executable file" }
+
+	get-childitem $PathToExe | % {$_.VersionInfo} | Select *
+	exit 0 # success
+} catch {
+	" Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
