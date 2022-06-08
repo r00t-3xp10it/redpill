82977ea53159de9b11f6a38a4310a3ee16e8e83e
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2022-06-08 20:51:49 +0100

    redpill v1.2.6 - RedTeam database library

--- lib/Misc-CmdLets/progressbar.ps1
+++ lib/Misc-CmdLets/progressbar.ps1
@@ -1,30 +1,48 @@
-﻿
-$Action = "Processes"
+<#
+.SNOPSIS
+   Author: @r00t-3xp10it
+
+.DESCRIPTION
+   Example how to spawn a PROGRESS BAR in pure powershell
+
+.EXAMPLE
+   PS C:\> .\progressbar.ps1 -Action 'Processes'
+
+.EXAMPLE
+   PS C:\> .\progressbar.ps1 -Action 'NetAdapters'
+#>
 
 
-   $CurrentItem = 0      #ProgressBar
-   $PercentComplete = 0  #ProgressBar
-
-   If($Action -ieq "Processes")
-   {
-      $GuiTitle = "Listing Procesess running ..."
-      $TerminalDisplay = Get-Process -Name *
-   }
-   Else
-   {
-      $GuiTitle = "Listing NetAdapters running ..."
-      $TerminalDisplay = Get-NetAdapter -Name * -IncludeHidden | Select-Object Status,Name,LinkSpeed,ifDesc,DriverName,DriverInformation  #* # List Net Adapters
-   }
-
-   $TotalItems = $TerminalDisplay.Count #ProgressBar
-   ForEach($VM in $TerminalDisplay)
-   {
-      $CurrentItem++
-      Write-Progress -Activity "$GuiTitle" -Status "$PercentComplete% Complete:" -PercentComplete $PercentComplete
-      $PercentComplete = [int](($CurrentItem / $TotalItems) * 100)
-      Start-Sleep -Milliseconds 50
-   }
+#CmdLet Global variable declarations!
+[CmdletBinding(PositionalBinding=$false)] param(
+   [string]$Action="Processes"
+)
 
 
-   #Display results using GUI
-   echo $TerminalDisplay|Out-GridView -Title "$GuiTitle"
+$CurrentItem = 0      #ProgressBar
+$PercentComplete = 0  #ProgressBar
+
+If($Action -ieq "Processes")
+{
+   $GuiTitle = "Listing Procesess running ..."
+   $TerminalDisplay = Get-Process -Name *
+}
+Else
+{
+   $GuiTitle = "Listing NetAdapters running ..."
+   $TerminalDisplay = Get-NetAdapter -Name * -IncludeHidden | Select-Object Status,Name,LinkSpeed,ifDesc,DriverName,DriverInformation  #* # List Net Adapters
+}
+
+
+$TotalItems = $TerminalDisplay.Count #ProgressBar
+ForEach($VM in $TerminalDisplay)
+{
+   $CurrentItem++
+   Write-Progress -Activity "$GuiTitle" -Status "$PercentComplete% Complete:" -PercentComplete $PercentComplete
+   $PercentComplete = [int](($CurrentItem / $TotalItems) * 100)
+   Start-Sleep -Milliseconds 50
+}
+
+
+#Display results using GUI
+echo $TerminalDisplay|Out-GridView -Title "$GuiTitle"
