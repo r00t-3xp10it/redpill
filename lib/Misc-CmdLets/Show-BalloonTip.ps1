cc4b83f61f5106897c690d9300d7cce177c10a47
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2023-12-20 04:50:16 +0000

    redpill v1.2.6 - RedTeam database library

--- lib/Misc-CmdLets/Show-BalloonTip.ps1
+++ lib/Misc-CmdLets/Show-BalloonTip.ps1
@@ -6,31 +6,46 @@
    Tested Under: Windows 10 (19044) x64 bits
    Required Dependencies: System.Windows.Forms
    Optional Dependencies: none
-   PS cmdlet Dev version: v1.0.1
+   PS cmdlet Dev version: v1.0.2
 
 .NOTES
    More attractive pop-up messages (balloon tips) may be displayed in Windows 7, 8.1 & 10
    through the Windows Forms API. The following PowerShell code will show a pop-up message
    next to the Windows 10 Notification bar that will automatically disappear in 20 seconds.
 
-.Parameter BalloonTipTitle
+.Parameter Title
    The ballontip title
 
-.Parameter BalloonTipText
+.Parameter Text
    The ballontip text
 
-.Parameter BalloonClose
-   Close ballontip after (default: 20000)
+.Parameter Icon
+   The ballontip icon (error|warning)
+
+.Parameter AutoClose
+   Close ballontip in (default: 20000)
+
+.EXAMPLE
+   PS C:\> .\Show-BallonTip.ps1 -title 'kernel error' -text 'a system error occour' -icon 'error' -autoclose '10000'
+
+.INPUTS
+   None. You cannot pipe objects into Show-BallonTip.ps1
+
+.OUTPUTS
+   none outputs available
 
 .LINK
+   https://github.com/r00t-3xp10it/redpill
+   https://github.com/r00t-3xp10it/meterpeter
    https://woshub.com/popup-notification-powershell
 #>
 
 
 [CmdletBinding(PositionalBinding=$false)] param(
-   [string]$BalloonTipText="A virus has detected in $Env:COMPUTERNAME",
-   [string]$BalloonTipTitle="Attention $Env:USERNAME",
-   [int]$BalloonClose='20000'
+   [string]$Text="A virus has detected in $Env:COMPUTERNAME",
+   [string]$Title="Attention $Env:USERNAME",
+   [string]$Icon="Warning",
+   [int]$AutoClose='20000'
 )
 
 
@@ -48,13 +63,28 @@ Try{
 
    ## Build ballon box
    $balmsg.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
-   $balmsg.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Warning
-   $balmsg.BalloonTipText = $BalloonTipText
-   $balmsg.BalloonTipTitle = $BalloonTipTitle
+
+   If($Icon -imatch '^(Error)$')
+   {
+      $balmsg.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Error   
+   }
+   Else
+   {
+      $balmsg.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Warning
+   }
+
+   $balmsg.BalloonTipText = $Text
+   $balmsg.BalloonTipTitle = $Title
    $balmsg.Visible = $true
-   $balmsg.ShowBalloonTip($BalloonClose)
+   $balmsg.ShowBalloonTip($AutoClose)
+
+   ## get rid of the following two lines if you don't want the tray-icon to disappear
+   # after xxxxx Milliseconds the icon will disappear however as soon as you mouseover it.
+   Start-Sleep -Milliseconds $AutoClose
+   $balmsg.dispose()
 }
 Catch
 {
    write-host "`n   > Error: $_ `n" -foregroundcolor Red -BackgroundColor Black
 }
+exit
